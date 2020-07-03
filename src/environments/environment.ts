85f65094987400885e6425fce3662fe368cdfd18
Swapnil <email2swap@gmail.com>
2020-07-03 20:45:57 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyBA4y5Lm-oK0nAPk9Pdz9k6KT6fq9N4nGE",
-    authDomain: "life-time-list.firebaseapp.com",
-    databaseURL: "https://life-time-list.firebaseio.com",
-    projectId: "life-time-list",
-    storageBucket: "life-time-list.appspot.com",
-    messagingSenderId: "556548776200",
-    appId: "1:556548776200:web:292681a2fd9ca60a65ccbd",
-    measurementId: "G-0R3Y5W8F1B"
-  }  
-};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
